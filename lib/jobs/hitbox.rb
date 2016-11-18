d24024c4f87caef8b8d09834506ba665f4b40555
Ellen Marie Dash <me@duckie.co>
2016-11-18 17:14:35 -0500

    Clean up Jobs::Hitbox.

--- lib/jobs/hitbox.rb
+++ lib/jobs/hitbox.rb
@@ -8,11 +8,11 @@ class PuppyRun
 
       # Use the Hitbox API to check if I'm streaming.
       # Set @@is_streaming to the result.
-      def is_streaming!
+      def check_if_streaming!
         response = Tessellator::Fetcher.new.call('get', 'https://api.hitbox.tv/user/duckinator')
         is_live = JSON.parse(response.body)['is_live']
 
-        is_live == '1'
+        @@is_streaming = (is_live == '1')
       end
 
       # Check if we're streaming (cached).
@@ -33,7 +33,7 @@ class PuppyRun
       def spawn_loop!
         Thread.new do
           loop do
-            @@is_streaming = Hitbox.is_streaming!
+            check_if_streaming!
             sleep 5 * 60 # 5 minutes.
           end
         end
