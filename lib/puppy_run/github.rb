02c5616fa73ad71ec80a589731525302bae6ee75
Ellen Marie Dash <me@duckie.co>
2016-11-26 00:54:57 -0500

    Handle complete lack of changelog, not just no entry in the changelog.

--- lib/puppy_run/github.rb
+++ lib/puppy_run/github.rb
@@ -8,13 +8,17 @@ class PuppyRun
       def changelog(repo, tag)
         changelog_file = get_changelog_file(repo)
 
+        if changelog_file.nil?
+          return '<p>No changelog found.</p>'
+        end
+
         raw_changelog = get("https://raw.githubusercontent.com/#{repo}/#{tag}/#{changelog_file}")
 
         separator = /## \[#{tag.gsub(/^v(\d)/, 'v?\1').gsub('.', '\\.')}\]/
         parts = raw_changelog.split(separator)
 
         if parts.length == 1
-          return '<p>No change log found.</p>'
+          return '<p>No entry found in changelog.</p>'
         end
 
         changelog = parts.last.split("\n## ").first
@@ -30,7 +34,11 @@ class PuppyRun
       def get_changelog_file(repo)
         files = JSON.parse(get("https://api.github.com/repos/#{repo}/contents/"))
 
-        files.find { |f| f['name'] =~ /^changelog/i }['name']
+        file = files.find { |f| f['name'] =~ /^changelog/i }
+
+        return nil if file.nil?
+
+        file['name']
       end
     end
   end
